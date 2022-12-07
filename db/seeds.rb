f71760239073ebc4273a3d8c0ec85a3bdeb95d13
karam084 <karam.ezz84@gmail.com>
2022-12-07 12:28:07 +0200

    Add real search app

---
+++ db/seeds.rb
@@ -0,0 +1,28 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+User.create(email: 'karam@example.com',
+            password: 'password',
+            password_confirmation: 'password',
+            name: 'Karam',
+            role: User.roles[:admin])
+User.create(email: 'fouad@gmail.com',
+            password: 'password',
+            password_confirmation: 'password',
+            name: 'Karam Fouad')
+
+10.times do |x|
+  post = Post.create(title: "Title #{x}",
+                     body: "Body #{x} Words go here Idk",
+                     user_id: User.first.id)
+
+  5.times do |y|
+    post.comments.create(body: "Comment #{y}",
+                   user_id: User.second.id)
+  end
+end
