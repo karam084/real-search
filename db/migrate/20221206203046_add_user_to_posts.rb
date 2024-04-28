9de35827293324e086799524a36db9cceff2f8a7
karam084 <karam.ezz84@gmail.com>
2024-04-28 23:45:11 +0300

    Add users

--- db/migrate/20221206203046_add_user_to_posts.rb
+++ db/migrate/20221206203046_add_user_to_posts.rb
@@ -1,5 +1,14 @@
 class AddUserToPosts < ActiveRecord::Migration[7.0]
   def change
     add_reference :posts, :user, null: false, foreign_key: true
+
+    reversible do |dir|
+      dir.up do
+        user = User.first
+        Post.all.each do |post|
+          post.update(user_id: user.id)
+        end
+      end
+    end
   end
 end
